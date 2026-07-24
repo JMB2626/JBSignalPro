557d5766eee35e0b8c6d06338bb928c08ecc3135
JMB2626 <Johnbaruani66@gmail.com>
2026-07-24 22:05:58 +0200

    Modify home route and port handling
    Update home route to check for BOT_TOKEN environment variable and adjust port configuration.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "JBSignalPro est en ligne !"
+    token = os.getenv("BOT_TOKEN")
+
+    if token:
+        return "JBSignalPro est connecté à Telegram."
+    else:
+        return "BOT_TOKEN introuvable."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
