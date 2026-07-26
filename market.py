4090d7939a2795aebfb7c2658dcc46a6d87b4160
JMB2626 <Johnbaruani66@gmail.com>
2026-07-26 07:39:05 +0200

    Check for TWELVEDATA_API_KEY before initializing TDClient
    Refactor TDClient initialization to check for API key availability.

--- market.py
+++ market.py
@@ -1,14 +1,18 @@
 from twelvedata import TDClient
 import os
 
-td = TDClient(apikey=os.getenv("TWELVEDATA_API_KEY"))
-
 def get_data(symbol):
+    api_key = os.getenv("TWELVEDATA_API_KEY")
+
+    if not api_key:
+        raise Exception("TWELVEDATA_API_KEY introuvable")
+
+    td = TDClient(apikey=api_key)
+
     ts = td.time_series(
         symbol=symbol,
         interval="5min",
         outputsize=100,
-        timezone="UTC"
     )
 
     return ts.as_pandas()
