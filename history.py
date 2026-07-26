7f5626bbc1636278716bdc1b3342c6c0c020b61f
JMB2626 <Johnbaruani66@gmail.com>
2026-07-26 22:59:50 +0200

    Update save_signal function to include signal and entry

--- history.py
+++ history.py
@@ -3,12 +3,28 @@ import os
 
 FILE = "history.csv"
 
-def save_signal(ema20, ema50, ema200, rsi, result=-1):
+def save_signal(ema20, ema50, ema200, rsi, signal, prix_entree):
     if not os.path.exists(FILE):
         with open(FILE, "w", newline="") as f:
             writer = csv.writer(f)
-            writer.writerow(["ema20", "ema50", "ema200", "rsi", "result"])
+            writer.writerow([
+                "ema20",
+                "ema50",
+                "ema200",
+                "rsi",
+                "signal",
+                "entry",
+                "result"
+            ])
 
     with open(FILE, "a", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow([ema20, ema50, ema200, rsi, result])
+        writer.writerow([
+            ema20,
+            ema50,
+            ema200,
+            rsi,
+            signal,
+            prix_entree,
+            -1
+        ])
