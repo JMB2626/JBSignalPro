c0e50a851bf5d326f7f3f798c6f4876a42eeaf1f
JMB2626 <Johnbaruani66@gmail.com>
2026-07-26 12:18:13 +0200

    Add main block to run the application

--- main.py
+++ main.py
@@ -29,3 +29,6 @@ def home():
             print(e)
 
     return "Analyse terminée."
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
