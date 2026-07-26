7d092b8bc7a542d837753c271b5f96eba5e7f6f9
JMB2626 <Johnbaruani66@gmail.com>
2026-07-26 23:17:52 +0200

    Create check_results function in result_checker.py
    Added a function skeleton for checking results.

---
+++ result_checker.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def check_results():
+    # À compléter : récupération du prix futur,
+    # comparaison avec le prix d'entrée,
+    # mise à jour de la colonne "result",
+    # puis appel à train()
+    pass
