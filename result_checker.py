107561436519e03bd34d2b3a4bf71045043e33f8
JMB2626 <Johnbaruani66@gmail.com>
2026-07-26 23:21:26 +0200

    Complete check_results function for trading signals
    Implement result checking and update logic in check_results function.

--- result_checker.py
+++ result_checker.py
@@ -1,8 +1,28 @@
 import pandas as pd
+from market import get_data
+from trainer import train
 
 def check_results():
-    # À compléter : récupération du prix futur,
-    # comparaison avec le prix d'entrée,
-    # mise à jour de la colonne "result",
-    # puis appel à train()
-    pass
+
+    df = pd.read_csv("history.csv")
+
+    for i in df.index:
+
+        if df.loc[i, "result"] != -1:
+            continue
+
+        signal = df.loc[i, "signal"]
+        entry = df.loc[i, "entry"]
+
+        market = get_data("EUR/USD", "5min")
+        last = market.iloc[-1]["close"]
+
+        if signal == "BUY":
+            df.loc[i, "result"] = 1 if last > entry else 0
+
+        if signal == "SELL":
+            df.loc[i, "result"] = 1 if last < entry else 0
+
+    df.to_csv("history.csv", index=False)
+
+    train()
