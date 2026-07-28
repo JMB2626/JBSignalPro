8f5d28a880752c3e1c64c593e0b1ef8fea438867
JMB2626 <Johnbaruani66@gmail.com>
2026-07-28 22:05:03 +0200

    Update market.py

--- market.py
+++ market.py
@@ -2,29 +2,41 @@ import os
 import requests
 import pandas as pd
 
-API_KEY = os.getenv("FINNHUB_API_KEY")
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
 
 def get_data():
 
     url = (
-        f"https://finnhub.io/api/v1/forex/candle"
-        f"?symbol=OANDA:XAU_USD"
-        f"&resolution=5"
-        f"&count=30"
-        f"&token={API_KEY}"
+        "https://www.alphavantage.co/query"
+        "?function=TIME_SERIES_INTRADAY"
+        "&symbol=XAUUSD"
+        "&interval=5min"
+        "&outputsize=compact"
+        f"&apikey={API_KEY}"
     )
 
     data = requests.get(url).json()
 
-    if data.get("s") != "ok":
+    series = data.get("Time Series FX (5min)")
+
+    if not series:
         raise Exception(data)
 
-    df = pd.DataFrame({
-        "open": data["o"],
-        "high": data["h"],
-        "low": data["l"],
-        "close": data["c"],
-        "volume": data["v"]
+    df = pd.DataFrame.from_dict(
+        series,
+        orient="index"
+    )
+
+    df = df.rename(columns={
+        "1. open": "open",
+        "2. high": "high",
+        "3. low": "low",
+        "4. close": "close"
     })
+
+    df = df.astype(float)
+
+    df = df.sort_index()
 
     return df
