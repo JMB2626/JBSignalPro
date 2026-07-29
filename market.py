97a7809a16630f9112d636b960ccfbf9bea6cae3
JMB2626 <Johnbaruani66@gmail.com>
2026-07-29 07:35:03 +0200

    Update market.py

--- market.py
+++ market.py
@@ -1,42 +1,13 @@
+from twelvedata import TDClient
 import os
-import requests
-import pandas as pd
-
-API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
-
 
 def get_data():
+    td = TDClient(apikey=os.getenv("TWELVEDATA_API_KEY"))
 
-    url = (
-        "https://www.alphavantage.co/query"
-        "?function=TIME_SERIES_INTRADAY"
-        "&symbol=XAUUSD"
-        "&interval=5min"
-        "&outputsize=compact"
-        f"&apikey={API_KEY}"
+    ts = td.time_series(
+        symbol="XAU/USD",
+        interval="5min",
+        outputsize=30
     )
 
-    data = requests.get(url).json()
-
-    series = data.get("Time Series FX (5min)")
-
-    if not series:
-        raise Exception(data)
-
-    df = pd.DataFrame.from_dict(
-        series,
-        orient="index"
-    )
-
-    df = df.rename(columns={
-        "1. open": "open",
-        "2. high": "high",
-        "3. low": "low",
-        "4. close": "close"
-    })
-
-    df = df.astype(float)
-
-    df = df.sort_index()
-
-    return df
+    return ts.as_pandas()
