20c394a3ef40d932f285dbfaf87875d6811e07ab
JMB2626 <Johnbaruani66@gmail.com>
2026-08-06 21:30:13 +0200

    Add EMA indicators to trend analysis

--- trend.py
+++ trend.py
@@ -1,12 +1,19 @@
+from ta.trend import EMAIndicator
+
 def trend(df):
 
-    first = df["close"].iloc[0]
-    last = df["close"].iloc[-1]
+    ema20 = EMAIndicator(df["close"], window=20).ema_indicator()
+    ema50 = EMAIndicator(df["close"], window=50).ema_indicator()
 
-    if last > first:
+    prix = df["close"].iloc[-1]
+
+    ema20_last = ema20.iloc[-1]
+    ema50_last = ema50.iloc[-1]
+
+    if prix > ema20_last > ema50_last:
         return "BUY"
 
-    if last < first:
+    if prix < ema20_last < ema50_last:
         return "SELL"
 
     return "RANGE"
